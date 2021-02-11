0900821473abd07c8bf1b9df3008c84b7fda3c66
Markus Loide <markusloide@gmail.com>
2021-02-11 20:58:08 +0200

    Ping pong test

---
+++ radish_server_test.go
@@ -0,0 +1,30 @@
+package radish_test
+
+import (
+	"testing"
+
+	"github.com/Artamus/radish"
+	"github.com/go-redis/redis"
+)
+
+func TestRadishServer(t *testing.T) {
+	t.Run("it responds PONG to PING", func(t *testing.T) {
+		server, _ := radish.NewRadishServer(6379)
+		go func() {
+			server.Listen()
+		}()
+
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		})
+
+		got := rdb.Ping().Val()
+		want := "PONG"
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+}
